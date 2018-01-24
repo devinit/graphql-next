9da1ca6c03c39463b575a22d85ee016470a9317a
epicallan <epicallan.al@gmail.com>
2018-01-24 17:17:50 +0300

    start example

---
+++ example/src/resolver.ts
@@ -0,0 +1,7 @@
+module.exports = {
+    // Query: {
+    //     async dvptCooperation(_root, args, ctx) {
+    //        return ctx.modules.shared.dvptCooperation(args.country);
+    //     },
+    // }
+};
