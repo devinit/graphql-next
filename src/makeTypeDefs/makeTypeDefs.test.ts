93aafd9f09bdd1e29c62529284cde1784e1665b7
epicallan <epicallan.al@gmail.com>
2018-01-29 15:09:55 +0300

    annoying npm dependencies missmatch

--- src/makeTypeDefs/makeTypeDefs.test.ts
+++ src/makeTypeDefs/makeTypeDefs.test.ts
@@ -1,28 +1,19 @@
 import 'jest';
 import { GraphQLSchema, buildSchema } from 'graphql';
 import { generateTsFromGql, getTypeDefs} from './';
-import simpleSchema from './test/types';
-import { schemaToInterfaces } from '@gql2ts/from-schema';
 import * as prettyFormat from 'pretty-format';
 
 describe('gql Types to Typescript types', () => {
     // TODO: should work with union tyoes
-    it.skip('should return merged typedefs', async () => {
+    it('should return merged typedefs', async () => {
         const typeDefs = await getTypeDefs('**/*.gql');
         expect(prettyFormat(typeDefs)).toMatchSnapshot();
     });
-    it.skip('merged types should be convertable to a GraphQLSchema', async () => {
+    it('merged types should be convertable to a GraphQLSchema', async () => {
         const typeDefs = await getTypeDefs('**/*.gql');
         expect(buildSchema(typeDefs)).toBeInstanceOf(GraphQLSchema);
     });
-    it('merged types schema', async () => {
-        // const namespaceOpts = { ignoreTypeNameDeclaration: true};
-        const actual: string = schemaToInterfaces(simpleSchema, {
-            ignoredTypes: []
-          });
-        expect(prettyFormat(actual)).toMatchSnapshot();
-    });
-    it.skip('End to End test: should create typescript types from graphql files', async () => {
+    it('End to End test: should create typescript types from graphql files', async () => {
         const tsTypesNameSpace = await generateTsFromGql({globPattern: '**/*.gql'});
         expect(prettyFormat(tsTypesNameSpace)).toMatchSnapshot();
     });
