4ad6ea3f0941a4a4d089377274c7527714fcae95
epicallan <epicallan.al@gmail.com>
2018-01-29 16:44:53 +0300

    remove example folder

--- example/src/resolver.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // Query: {
-    //     async dvptCooperation(_root, args, ctx) {
-    //        return ctx.modules.shared.dvptCooperation(args.country);
-    //     },
-    // }
-};
